098e785796cfe7ae89186c95e2488bc46700ab07
andreikostsen <pks@tut.by>
2023-10-27 15:21:31 +0300

    form started

--- src/Form.jsx
+++ src/Form.jsx
@@ -20,7 +20,61 @@ import { GrMail } from 'react-icons/gr'
 import { BsFillCheckSquareFill } from 'react-icons/bs'
 
 export const Form = () => {
+  const methods = useForm()
+
+  const onSubmit = methods.handleSubmit(data => {
+    console.log(data)
+  })
+
   return (
-    <div className="text-center mt-5">Welcome to React Forms Tutorial!</div>
+    <FormProvider {...methods}>
+      <form
+        onSubmit={e => e.preventDefault()}
+        noValidate
+        className="container"
+      >
+        <div className="grid gap-5 md:grid-cols-2">
+          <Input
+            label="name"
+            name="name"
+            type="text"
+            id="name"
+            placeholder="type your name..."
+            validation={{
+              required: {
+                value: true,
+                message: 'required',
+              },
+            }}
+          />
+          <Input
+            label="password"
+            name="password"
+            type="password"
+            id="password"
+            placeholder="type your password..."
+            validation={{
+              required: {
+                value: true,
+                message: 'required',
+              },
+              minLength: {
+                value: 6,
+                message: 'min 6 characters',
+              },
+            }}
+          />
+        </div>
+        <div className="mt-5">
+          <button
+            onClick={onSubmit}
+            className="flex items-center gap-1 p-5 font-semibold text-white bg-blue-600 rounded-md hover:bg-blue-800"
+          >
+            <GrMail />
+            Submit Form
+          </button>
+        </div>
+      </form>
+    </FormProvider>
   )
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -12,12 +12,51 @@ import { useFormContext } from 'react-hook-form'
 import { AnimatePresence, motion } from 'framer-motion'
 import { MdError } from 'react-icons/md'
 
-export const Input = ({}) => {
-  return <div>input!</div>
+export const Input = ({ label, type, id, placeholder,  validation, name }) => {
+
+  const { register,
+    formState: { errors },
+  } = useFormContext()
+
+  const inputError = findInputError(errors, label)
+  const isInvalid = isFormInvalid(inputError)
+
+  return (
+    <div className="flex flex-col w-full gap-2">
+      <div className="flex justify-between">
+        <label htmlFor={id} className="font-semibold capitalize">
+          {label}
+        </label>
+        <AnimatePresence mode="wait" initial={false}>
+          {isInvalid && (
+            <InputError
+              message={inputError.error.message}
+              key={inputError.error.message}
+            />
+          )}
+        </AnimatePresence>
+      </div>
+      <input
+        id={id}
+        type={type}
+        className="w-full p-5 font-medium border rounded-md border-slate-300 placeholder:opacity-60"
+        placeholder={placeholder}
+        {...register(name, validation)}
+      />
+    </div>
+  )
 }
 
 const InputError = () => {
-  return <div>error</div>
+  return (
+    <motion.p
+      className="flex items-center gap-1 px-2 font-semibold text-red-500 bg-red-100 rounded-md"
+      {...framer_error}
+    >
+      <MdError />
+      {message}
+    </motion.p>
+  )
 }
 
 const framer_error = {
